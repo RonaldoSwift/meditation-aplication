2f914669ba33949c3c70472d8339e959a7ef252e
Ronaldo Vargas <ronaldovh99@gmail.com>
2021-03-19 12:16:40 -0500

    Initial Commit

---
+++ meditation-aplication/meditation_aplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditation_aplicationApp.swift
+//  meditation-aplication
+//
+//  Created by Ronaldo Andre Vargas Huaman on 19/03/21.
+//
+
+import SwiftUI
+
+@main
+struct meditation_aplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
